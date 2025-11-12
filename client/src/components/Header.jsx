b300fcbcaf7b888d46489c7c7f85bf67641399db
rajanawasthi00 <rajanawasthi576@gmail.com>
2025-11-12 20:07:32 +0530

    hello sir

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="bg-cyan-800 text-white">
         <div className="headerContainer flex items-center justify-between max-w-[1200px] mx-auto px-4 py-2">
-            <Link to={"/"} className='font-bold text-2xl'>Brand Logo</Link>
+            <Link to={"/"} className='font-bold text-2xl'>Rahul mishra</Link>
             <ul className='flex gap-3'>
                 <li>
                     <Link to={"/"} className='hover:text-neutral-500'>Home</Link>
--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Topbar = () => {
   return (
-    <div className='text-center py-4 bg-emerald-300 font-bold '>Today All Products are 100% off</div>
+    <div className='text-center py-4 bg-emerald-300 font-bold '>Today All Products are 50% off</div>
   )
 }
 
