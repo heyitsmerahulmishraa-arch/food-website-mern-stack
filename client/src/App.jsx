061d1c5f7c63eded0dc278031b9d5909b1b98f3a
rajanawasthi00 <rajanawasthi576@gmail.com>
2025-11-14 00:05:49 +0530

    change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,21 @@ import Home from "./components/Home"
 import Header from "./components/Header"
 import Topbar from "./components/Topbar"
 import Footer from "./components/Footer"
+import AllProducts from "./components/AllProducts"
 
 function App() {
   return (
     <>
-    <Topbar/>
-    <Header/>
-    <Routes>
-        <Route path="/" element={<Home/>}/>
-    </Routes>
-    <Footer/>
+      <Topbar />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<AllProducts />} />
+      </Routes>
+
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App
--- client/src/components/Top4Product.jsx
+++ client/src/components/Top4Product.jsx
@@ -4,13 +4,19 @@ import { top4PdoructsList } from "../assets/assets";
 
 const Top4Product = () => {
   return (
-    <div className="container flex border flex-col my-[50px]">
+    <div className="container flex flex-col my-[50px]">
       <p className="text-center my-[40px] text-4xl">
         <b>Top 4 Products</b>
       </p>
-          <div className="top-pro-container flex gap-[32px]">
-              {top4PdoructsList.map((el) => (<ProductCard tittle={el.title} price={el.price} image={el.thumbnail} />))}
-          </div>
+      <div className="top-pro-container grid grid-cols-4 gap-[32px]">
+        {top4PdoructsList.map((el) => (
+          <ProductCard
+            tittle={el.title}
+            price={el.price}
+            image={el.thumbnail}
+          />
+        ))}
+      </div>
     </div>
   );
 };
