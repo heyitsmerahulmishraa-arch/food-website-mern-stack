ac4c25a66e228a0a22dcf0f4234aa45e9037aedf
rajanawasthi00 <rajanawasthi576@gmail.com>
2025-11-14 19:56:52 +0530

    push

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 const Topbar = () => {
   return (
-<<<<<<< HEAD
-    <div className='container bg-[var(--top-banner-bg)] text-[var(--text-main-color)] text-center py-4 font-bold '>🛒 Limited Time Offer! Enjoy 100% OFF on All Food Products Today Only!</div>
-=======
     <div className='container bg-[var(--top-banner-bg)] text-[var(--text-main-color)] text-center py-4 font-bold '>🛒 Limited Time Offer! Enjoy 49% OFF on All Food Products Today Only!</div>
->>>>>>> rajan
   )
 }
 
